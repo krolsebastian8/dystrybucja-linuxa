1b2f3033d844eb139270cf47e22ede56bdfaa2bc
Wojciech Kuśmierczyk <wojciech.kusmierczyk@outlook.com>
2017-11-29 18:12:38 +0100

    Refactoring and add main loop with case statement

--- skrypt_uruchomieniowy/skrypt.sh
+++ skrypt_uruchomieniowy/skrypt.sh
@@ -2,29 +2,45 @@
 
 #Configuration
 
-#Configuration of input stream
-O_STREAM="/tmp/input.stream"
->$O_STREAM
+#Configuration of output stream
+O_STREAM="/tmp/output.stream"
 
-#Powitanie
-#dialog --title 'Konfigurator Środowiska Programisty Frontend' --msgbox 'Zaczynamy!' 5 80
-dialog --inputbox "Podaj swoje imię: " 8 50 2>$O_STREAM
+function getNameFromUser(){
+	>$O_STREAM
+	dialog --inputbox "Podaj swoje imię: " 8 50 2>$O_STREAM
+	name=$(<$O_STREAM)
+}
 
-name=$(<$O_STREAM)
-
-function configuration(){
-	dialog --title "Konfiguracja oprogramowania" --menu "Witaj $name!\nSkonfiguruj swoje oprogramowanie:" 15 50 5 \
+function chooseTheConfigurationOption(){
+	>$O_STREAM
+	dialog --title "Konfiguracja oprogramowania" --menu "Witaj $1!\nSkonfiguruj swoje oprogramowanie:" 15 50 5 \
 		1 "Konfiguracja GitHub'a" \
 		2 "Konfiguracja Bazy danych" \
 		3 "Konfiguracja Serwera Apache" \
 		4 "Node.js" \
-		5 "Wybór IDE"
+		5 "Wybór IDE" 2>$O_STREAM
+	chosenOption=$(<$O_STREAM)
 }
 
+function configuration(){
+	getNameFromUser
+	
+	while [ true ] ; do
+	clear
+	chooseTheConfigurationOption $name
 
+	case $chosenOption in
+  		1) echo "GitHub" ;;
+		2) echo "Baza danych" ;;
+  		3) echo "Serwer Apache" ;;
+		4) echo "Node.js" ;;
+		5) echo "Wybór IDE" ;;
+  		*) break 
+	esac	
+	done
+}
 
 configuration
-#echo $userName
 
 rm $O_STREAM
 clear
